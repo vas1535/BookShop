17c6fb5d4a0f1c9bc4b88a644636389196b5d85f
user-2304 <user-2304@DESKTOP-H46TBLK>
2022-10-02 22:08:57 +0300

    Add project files.

---
+++ BookShop/Program.cs
@@ -0,0 +1,189 @@
+﻿//BookShop
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+namespace BookShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> list = new List<Book>();
+            list.Add(new Book("Оно", "America", "Horror", "Stephen King", 700, 1995, 1000));
+            list.Add(new Book("Оно2", "America", "Horror", "Stephen King", 700, 1995, 1000));
+            list.Add(new Book("Оно3", "America", "Horror", "Stephen King", 700, 1995, 1000));
+            Shop sh = new Shop(Properties.Resources.ShopName, "ТЦ \"Рассвет\"", list);
+            sh.GetShopName();
+            Command.HelpCommand();
+            while (true)
+            {
+                Console.Write("Введите новую команду: ");
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "help": Command.HelpCommand(); break;
+                    case "addbook": sh.AddABook(Command.AddBookCommand()); sh.GetAllBooks(); break;
+                    case "removebook": sh.DeleteBookByIndex(Command.RemoveBookCommand()); sh.GetAllBooks(); break;
+                    case "removebookname": sh.DeleteBookByName(Command.RemoveBookCommandName()); sh.GetAllBooks(); break;
+                    case "getbooks": Command.GetAllBookCommand(); sh.GetAllBooks(); break;
+                    case "clear": Console.Clear(); break;
+                    default: Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Не удалось распознать команду. Наберите help для списка команд"); Console.ForegroundColor = ConsoleColor.White; break;
+                }
+
+            }
+        }
+    }
+    class Book : IEnumerable, IComparable, IBook
+    {
+        public string Name { get; set; }
+        public string Publishing { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+        public int NumberPages { get; set; }
+        public int YearOfPublishing { get; set; }
+        public int Price { get; set; }
+
+        public Book(string name, string publishing, string genre, string author, int numberpages, int yearofpublishing, int price)
+        {
+            Name = name;
+            Publishing = publishing;
+            Genre = genre;
+            Author = author;
+            NumberPages = numberpages;
+            YearOfPublishing = yearofpublishing;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return Name + " " + Publishing + " " + Genre + " " + Author + " " + NumberPages.ToString() + " " +
+                YearOfPublishing.ToString() + " " + Price.ToString();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)Name).GetEnumerator();
+        }
+
+        public int CompareTo(object obj)
+        {
+            return Name.CompareTo((string)obj);
+        }
+
+        public int Compare(object obj, object obj2)
+        {
+            return ((Book)obj).Name.CompareTo((string)obj2);
+        }
+    }
+
+    class Shop : IShop
+    {
+        List<Book> Books;
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public Shop(string name, string address, List<Book> books)
+        {
+            Name = name;
+            Address = address;
+            Books = books;
+        }
+        public void GetShopName()
+        {
+            Console.WriteLine("Добро пожаловать в {0} по адресу {1}", Name, Address);
+        }
+        public void GetAllBooks()
+        {
+            int index = 1;
+            foreach (Book book in Books)
+            {
+                Console.WriteLine("Индекс: {0} {1}", index, book);
+                index++;
+            }
+        }
+        public void AddABook(Book book)
+        {
+            Books.Add(book);
+        }
+        public void DeleteBookByName(string name)
+        {
+            int index = -1;
+            foreach (Book book in Books)
+            {
+                if (book.CompareTo(name) == 0) index = Books.IndexOf(book);
+            }
+            if (index >= 0) Books.RemoveAt(index);
+        }
+        public void DeleteBookByIndex(int index)
+        {
+            Books.RemoveAt(index - 1);
+        }
+    }
+
+    class Command
+    {
+        public static void HelpCommand()
+        {
+            Console.WriteLine("Используйте addbook для добавления книги, removebook для удаления книги по индексу, removebookname для удаления книги по имени,getbooks для списка книг, clear для очистки консоли:");
+        }
+        public static Book AddBookCommand()
+        {
+            Book book;
+            Console.Write("Введите имя книги: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите издательство книги: ");
+            string publishing = Console.ReadLine();
+            Console.Write("Введите жанр книги: ");
+            string genre = Console.ReadLine();
+            Console.Write("Введите автора книги: ");
+            string author = Console.ReadLine();
+            Console.Write("Введите количество страниц книги: ");
+            int numberpage = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите год издания книги: ");
+            int yearofpublishing = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите цену книги: ");
+            int price = Int32.Parse(Console.ReadLine());
+            book = new Book(name, publishing, genre, author, numberpage, yearofpublishing, price);
+            return book;
+        }
+        public static int RemoveBookCommand()
+        {
+            Console.WriteLine("Укажите индекс удаляемой книги:");
+            return Int32.Parse(Console.ReadLine());
+
+        }
+        public static string RemoveBookCommandName()
+        {
+            Console.WriteLine("Укажите имя удаляемой книги");
+            return Console.ReadLine();
+        }
+        public static void GetAllBookCommand()
+        {
+            Console.WriteLine("Список книг в магазине:");
+        }
+    }
+
+    interface IShop
+    {
+
+        string Name { get; set; }
+        string Address { get; set; }
+        public void GetShopName();
+
+        public void GetAllBooks();
+        public void AddABook(Book book);
+        public void DeleteBookByName(string name);
+        public void DeleteBookByIndex(int index);
+    }
+
+    interface IBook
+    {
+        public string Name { get; set; }
+        public string Publishing { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+        public int NumberPages { get; set; }
+        public int YearOfPublishing { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ HomeWork1/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//#define TASK1
+//#define TASK2
+//#define TASK3
+//#define TASK4
+//#define TASK5
+//#define TASK6
+//#define TASK7
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if TASK1
+            int number = 0;
+            Console.WriteLine("Введите число в диапазоне от 1 до 100: ");
+            try
+            {
+                number = Int32.Parse(Console.ReadLine());
+                if (number < 0 || number > 100)
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Неверные данные! Необходимо ввести число от 1 до 100!");
+            }
+            if (number > 0 && number < 100)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                    return;
+                }
+                if (number % 3 == 0) Console.WriteLine("Fizz");
+                if (number % 5 == 0) Console.WriteLine("Buzz");
+                if (number % 3 != 0 && number % 5 != 0) Console.WriteLine(number);
+            }
+#endif
+#if TASK2
+            int number = 0;
+            int percent = 0;
+
+            Console.WriteLine("Введите число: ");
+            try
+            {
+                number = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Введите число для процента: ");
+                percent = Int32.Parse(Console.ReadLine());
+                int result = number * percent / 100;
+                Console.WriteLine("Результат вычисления: {0}", result);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Вы ввели неверные символы! Введите число");
+
+            }
+#endif
+#if TASK3
+            double num1, num2, num3, num4;
+            int result;
+
+            Console.WriteLine("Введите 1 цифру");
+            num1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите 2 цифру");
+            num2 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите 3 цифру");
+            num3 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите 4 цифру");
+            num4 = Double.Parse(Console.ReadLine());
+
+            result = (int)num1 * 1000 + (int)num2 * 100 + (int)num3 * 10 + (int)num4;
+            Console.WriteLine("Полученный результат = {0}", result);
+#endif
+#if TASK4
+            int size = 6;
+            int[] arr = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+            int x1=0, x2=0;
+            Console.WriteLine("Введите первый номер разряда: ");
+            try
+            {
+                x1 = Int32.Parse(Console.ReadLine());
+                if (x1 < 1 || x1 > 6) throw new Exception();
+            }
+            catch (Exception) { Console.WriteLine("Неправильно! Введите разряд от 1 от 6!");}
+            Console.WriteLine("Введите второй номер разряда: ");
+            try
+            {
+                x2 = Int32.Parse(Console.ReadLine());
+                if (x2 < 1 || x2 > 6) throw new Exception();
+            }
+            catch (Exception) { Console.WriteLine("Неправильно! Введите разряд от 1 от 6!");}
+
+            int buffer2 = arr[x1 - 1];
+            arr[x1 - 1] = arr[x2 - 1];
+            arr[x2 - 1] = buffer2;
+            foreach (int i in arr)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+#endif
+#if TASK5
+            int year = 0, month = 0, day = 0;
+            Console.WriteLine("Введите год: ");
+            year = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите месяц: ");
+            try
+            {
+                month = Int32.Parse(Console.ReadLine());
+                if (month < 1 || month > 12) throw new Exception();
+            }
+            catch (Exception) { Console.WriteLine("Неправильно! Введите корректный номер месяца");}                   
+          Console.WriteLine("Введите день: ");
+            try
+            {
+                day = Int32.Parse(Console.ReadLine());
+                if (day < 1 || day > 31) throw new Exception();
+            }
+            catch (Exception) { Console.WriteLine("Неправильно! Такого дня не существует"); }
+           
+            DateTime dateValue = new DateTime(year, month, day);
+            string season = "";
+            if (month == 12 || month <= 2) { season = "зима"; }
+            if (month >= 3 && month <= 5) { season = "весна"; }
+            if (month >= 6 && month <= 8) { season = "лето"; }
+            if (month >= 9 && month <= 11) { season = "осень"; }
+            Console.WriteLine(dateValue.ToString("dddd") + ", " + season);
+#endif
+#if TASK6
+            Console.WriteLine("Введите температуру: ");
+            int t = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите температурную шкалу (F или C): ");
+            string str = Console.ReadLine();
+
+            double temp = 0;
+            if (str == "F")
+            {
+                temp = 1.8 * t + 32;
+                Console.WriteLine(t + " градусов по Цельсию = {0} градусов по Форенгейту", temp);
+            }
+            if (str == "C")
+            {
+                temp = (t - 32) / 1.8;
+                Console.WriteLine(t + " градусов по Форенгейту = {0} градусов по Цельсию", temp);
+            }
+#endif
+#if TASK7
+            Console.WriteLine("Введите верхнюю границу диапазона: ");
+            int x1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите нижнюю границу диапазона: ");
+            int x2 = Int32.Parse(Console.ReadLine());
+            if (x1 > x2)
+            {
+                int buffer = x1;
+                x1 = x2;
+                x2 = buffer;
+            }
+            Console.WriteLine("Все четные числа в указанном диапазоне: ");
+            for (int i = x1; i <= x2; i++)
+            {
+                if (i % 2 == 0) Console.Write(i + " ");              
+            }
+#endif
+        }
+    }
+}
+
